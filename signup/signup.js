d91a5adde83228d6ad1acc3bf327cdbe816d3708
Esther Cheng <esther588@berkeley.edu>
2022-04-10 22:48:33 -0700

    signup errors

---
+++ signup/signup.js
@@ -0,0 +1,20 @@
+const signupForm = document.getElementById("signup-form");
+const signupButton = document.getElementById("signup-form-submit");
+const signupBlankError = document.getElementById("signup-blank-error");
+const signupPwdError = document.getElementById("signup-pwd-error");
+
+signupButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const firstname = signupForm.firstname.value;
+    const lastname = signupForm.lastname.value;
+    const username = signupForm.username.value;
+    const password = signupForm.password.value;
+
+    if (firstname == "" && lastname == "" && username === "" && password === "") {
+        signupBlankError.style.opacity = 1;
+    } else if (password.length < 8) {
+        signupPwdError.style.opacity = 1;
+    } else {
+        alert("You have successfully signed up!");
+    }
+})
