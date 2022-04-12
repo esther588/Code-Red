4ed5944f01684061a517f6f184b98a0a2820ac53
Esther Cheng <esther588@berkeley.edu>
2022-04-12 16:57:52 -0700

    display returned value in js in html file

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -3,3 +3,5 @@ function getFirstName() {
     var storedFirst = localStorage.getItem(username + ' firstname');
     return storedFirst.value;
 }
+
+document.getElementById("ret").innerHTML = getFirstName();
