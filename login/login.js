dda7e60b9d62ca571a73397c86fc4cfa0a6b06e6
Esther Cheng <esther588@berkeley.edu>
2022-04-11 19:18:25 -0700

    adjust check credentials code

--- login/login.js
+++ login/login.js
@@ -4,31 +4,19 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
-
-function checkCred() {
-
     // Stored data from the signup-form
     var storedUser = localStorage.getItem('username');
     var storedPwd = localStorage.getItem('password');
 
     // Entered data from the login-form
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
 
     // check if stored data from register-form is equal to data from login form
     if(username.value == storedUser && password.value == storedPwd) {
-        alert('You are loged in.');
+        alert("You have successfully logged in.");
+        location.reload();
     }else {
-        alert('ERROR.');
+        loginErrorMsg.style.opacity = 1;
     }
-}
+})
