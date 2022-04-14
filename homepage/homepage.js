1312f161f5e2aaba35f81f376257e5cf11e0b6da
Esther Cheng <esther588@berkeley.edu>
2022-04-14 09:27:48 -0700

    directly write value in html file

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -3,5 +3,3 @@ function getFirstName() {
     var storedFirst = localStorage.getItem(username + ' firstname');
     return storedFirst.value;
 }
-
-document.getElementById("ret").innerHTML = getFirstName();
