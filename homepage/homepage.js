8ee9247d1c6f3b7fb3d8583eb272730dcd67d345
Esther Cheng <esther588@berkeley.edu>
2022-04-16 16:13:27 -0700

    revert

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -11,11 +11,11 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function getDayOfWeek() {
-    return String(days[dayOfWeek]);
+    return days[dayOfWeek];
 }
 
 function getYear() {
-    return String(currentYear);
+    return currentYear;
 }
 
 function daysInMonth(month, year) {
