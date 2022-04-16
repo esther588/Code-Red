03e441500d7eb094593cc2b012ce3d9d86ae0ac6
Esther Cheng <esther588@berkeley.edu>
2022-04-16 15:34:01 -0700

    create array of days and index

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -7,13 +7,16 @@ dayOfWeek = currentDay.getDay();
 // Create array of months
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+// Create array of days
+days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 function getDayOfWeek() {
-    return dayOfWeek;
+    return days[dayOfWeek];
 }
 
 document.getElementById("dayOfWeek").innerHTML = getDayOfWeek();
 
-function daysInMonth(month,year) {
+function daysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
 }
             
