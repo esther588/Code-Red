6f91c61f68efc8489fdd65855be334bdd90ef91e
Esther Cheng <esther588@berkeley.edu>
2022-04-15 17:22:30 -0700

    edit calendar days

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -2,32 +2,26 @@
 currentDay = new Date();
 currentMonth = currentDay.getMonth();
 currentYear = currentDay.getFullYear();
+dayOfWeek = currentDay.getDay(); 
 
 // Create array of months
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-calendarDays(currentMonth, currentYear);
-
-function calendarDays(month, year) {
-    tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
+function calendarDays() {
     for( var i = 1; i <= 31; i += 1 ){
         var addClass = '';
         if( i === 12 ){ addClass = ' class="selected"'; }
         
         switch( i ){
-          case 8:
-          case 10:
-          case 27:
+        case 8:
+        case 10:
+        case 27:
             addClass = ' class="event"';
-          break;
+        break;
         }
 
         document.write( '<li><a href="#" title="'+i+'" data-value="'+_i+'"'+addClass+'>'+i+'</a></li>' );
-      }
-
+    }
 }
 
 function daysInMonth(month,year) {
