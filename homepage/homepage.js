e706e271b6e004953110477b68f0708429b016c3
Esther Cheng <esther588@berkeley.edu>
2022-04-16 15:45:13 -0700

    edit days array

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -8,7 +8,7 @@ dayOfWeek = currentDay.getDay();
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 // Create array of days
-days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function getDayOfWeek() {
     return days[dayOfWeek];
