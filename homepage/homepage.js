7a472c4661c41a4041d5a5eef5ce11e4f1449f9b
Esther Cheng <esther588@berkeley.edu>
2022-04-16 23:45:45 -0700

    include the date

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,8 +1,9 @@
-// Get today's date
-var currentDay = new Date();
-var currentMonth = currentDay.getMonth();
-var currentYear = currentDay.getFullYear();
-var dayOfWeek = currentDay.getDay(); 
+// Create a date object and get the date, month, year and day of the week
+var today = new Date();
+var currentDate = today.getDate();
+var currentMonth = today.getMonth();
+var currentYear = today.getFullYear();
+var dayOfWeek = today.getDay();
 
 // Create array of months
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -10,11 +11,14 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 // Create array of days
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-displayDates(days, dayOfWeek, currentYear);
+displayDates(days, dayOfWeek, currentDate, months, currentMonth, currentYear);
 
-function displayDates(daysArr, dayOfWeek, year) {
+function displayDates(daysArr, dayOfWeek, date, monthsArr, month, year) {
     finalDay = document.getElementById("currentDay");
     finalDay.innerHTML = daysArr[dayOfWeek];
+
+    fullDate = document.getElementById("currentDate");
+    finalYear.innerHTML = monthsArr[month] + " " + date;
 
     finalYear = document.getElementById("currentYear");
     finalYear.innerHTML = year;
