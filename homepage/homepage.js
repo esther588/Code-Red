b1e1c1d3ccbf5be0d4974ab2afa0e2b826d2085b
Esther Cheng <esther588@berkeley.edu>
2022-04-16 21:45:54 -0700

    edit into function

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -10,14 +10,14 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 // Create array of days
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-finalDay = document.getElementById("currentDay");
-finalDay.innerHTML = days[dayOfWeek];
+displayDates(days, dayOfWeek, currentYear);
 
-finalYear = document.getElementById("currentYear");
-finalYear.innerHTML = currentYear;
+function displayDates(daysArr, dayOfWeek, year) {
+    finalDay = document.getElementById("currentDay");
+    finalDay.innerHTML = daysArr[dayOfWeek];
 
-function getYear() {
-    return currentYear;
+    finalYear = document.getElementById("currentYear");
+    finalYear.innerHTML = year;
 }
 
 function daysInMonth(month, year) {
