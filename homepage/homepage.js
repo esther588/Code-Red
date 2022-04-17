7f3eede80bfb4f0b0686f781f53595ee0057aafa
Esther Cheng <esther588@berkeley.edu>
2022-04-17 01:02:12 -0700

    comment out code to test

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -14,11 +14,6 @@ days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satur
 displayDay(days, dayOfWeek, currentDate, months, currentMonth, currentYear);
 
 function displayDay(daysArr, dayOfWeek, date, monthsArr, month, year) {
-    finalDay = document.getElementById("currentDay");
-    finalDay.innerHTML = daysArr[dayOfWeek];
-
-    fullDate = document.getElementById("currentDate");
-    fullDate.innerHTML = monthsArr[month] + " " + date;
 
     finalYear = document.getElementById("currentYear");
     finalYear.innerHTML = year;
