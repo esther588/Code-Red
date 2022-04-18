4641700c7c96102bab04aac3ac87d88516bff745
Esther Cheng <esther588@berkeley.edu>
2022-04-17 20:31:21 -0700

    change calendar body code

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -14,7 +14,7 @@ days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satur
 displayDay(days, dayOfWeek);
 displayDate(currentDate, months, currentMonth);
 displayYear(currentYear);
-addDays();
+showCalendar(currentMonth, currentYear)
 
 function displayDay(daysArr, day) {
     finalDay = document.getElementById("currentDay");
@@ -32,24 +32,50 @@ function displayYear(year) {
     finalYear.innerHTML = year;
 }
 
-function addDays() {
-    var calendarBody = '';
-                    for(var i = 1; i <= 31; i += 1){
-                        var addClass = '';
-                        if( i === 12 ){ addClass = ' class="selected"'; }
-                        
-                        switch( i ){
-                        case 8:
-                        case 10:
-                        case 27:
-                            addClass = ' class="event"';
-                        break;
-                        }
+function showCalendar(month, year) {
 
-                        calendarBody += '<li><a href="#" title="'+i+'" data-value="'+i+'"'+addClass+'>'+i+'</a></li>';
-                    }
-                    finalBody = document.getElementById("createBody");
-                    finalBody.innerHTML = calendarBody;
+    let firstDay = (new Date(year, month)).getDay();
+
+    tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                cell = document.createElement("td");
+                cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth(month, year)) {
+                break;
+            }
+
+            else {
+                cell = document.createElement("td");
+                cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
 }
 
 function daysInMonth(month, year) {
