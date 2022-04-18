a93584fa991846e20e13ee44fc4569a2c3518f61
Esther Cheng <esther588@berkeley.edu>
2022-04-17 20:05:35 -0700

    create function to add body of calendar

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -34,4 +34,21 @@ function displayYear(year) {
 function daysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
 }
+
+function addDays() {
+    for( var i = 1; i <= 31; i += 1 ){
+        var addClass = '';
+        if( i === 12 ){ addClass = ' class="selected"'; }
+        
+        switch( i ){
+          case 8:
+          case 10:
+          case 27:
+            addClass = ' class="event"';
+          break;
+        }
+
+        document.write( '<li><a href="#" title="'+i+'" data-value="'+i+'"'+addClass+'>'+i+'</a></li>' );
+      }
+}
             
