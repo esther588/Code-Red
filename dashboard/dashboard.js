457100008004ff1a47b4b32b88a1a09ca00951ca
Esther Cheng <esther588@berkeley.edu>
2022-04-18 22:51:37 -0700

    include pie chart

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,3 +1,16 @@
+var radios = document.getElementsByName("radio");
+var isChecked = false;
+for ( var i = 0; i < radios.length; i++) {
+    if(radios[i].checked) {
+        var selected = radios[i].value;
+        if(typeof selected == "opt-4") {
+          createPie();
+        }
+        isChecked = true;
+        break;
+    }
+}
+
 function createPie() {
     var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
     var yValues = [55, 49, 44, 24, 15];
