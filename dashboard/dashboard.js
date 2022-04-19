8d700258bea7c9868806fee418ff14f3c6112eb8
Esther Cheng <esther588@berkeley.edu>
2022-04-18 22:31:22 -0700

    use canvas to create pie chart

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -6,27 +6,30 @@ if(document.getElementById("opt-1").checked) {
 }
 
 function createPie() {
-    let pieHtml = document.getElementById("pieChart");
-    var data = [
-        {x: "White", value: 223553265},
-        {x: "Black or African American", value: 38929319},
-        {x: "American Indian and Alaska Native", value: 2932248},
-        {x: "Asian", value: 14674252},
-        {x: "Native Hawaiian and Other Pacific Islander", value: 540013},
-        {x: "Some Other Race", value: 19107368},
-        {x: "Two or More Races", value: 9009073}
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
     ];
-  
-    // create the chart
-    var chart = anychart.pie();
-  
-    // set the chart title
-    chart.title("Population by Race for the United States: 2010 Census");
-  
-    // add the data
-    chart.data(data);
-  
-    // display the chart in the container
-    chart.container('container');
-    pieHtml.innerHTML = chart.draw();
+
+    new Chart("pieChart", {
+    type: "pie",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    options: {
+        title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+        }
+    }
+    });
 }
