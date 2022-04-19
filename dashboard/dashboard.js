9fe75e126ee31432d4650bac2add77b8b94e4dac
Esther Cheng <esther588@berkeley.edu>
2022-04-19 13:30:49 -0700

    use only one function to create graphs

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,3 +1,14 @@
+var typeOfGraph = "";
+
+if(document.getElementById('opt-1').checked) {
+} else if(document.getElementById('opt-2').checked) {
+    typeOfGraph = "line"
+} else if(document.getElementById('opt-3').checked) {
+    typeOfGraph = "bar"
+} else if(document.getElementById('opt-4').checked) {
+    typeOfGraph = "pie"
+}
+
 const ctx = document.getElementById("graph");
 const ctx1 = document.getElementById("graph");
 const ctx2 = document.getElementById("graph");
@@ -75,58 +86,7 @@ function createAll() {
     });
     }
     
-    function createLine() {
-        var xValues = [100,200,300,400,500,600,700,800,900,1000];
-        
-        new Chart(ctx, {
-            type: "line",
-            data: {
-            labels: xValues,
-            datasets: [{ 
-                data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-                borderColor: "red",
-                fill: false
-            }, { 
-                data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-                borderColor: "green",
-                fill: false
-            }, { 
-                data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-                borderColor: "blue",
-                fill: false
-            }]
-            },
-            options: {
-            legend: {display: false}
-            }
-        });
-    }
-  
-    function createBar() {
-        var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-        var yValues = [55, 49, 44, 24, 15];
-        var barColors = ["red", "green","blue","orange","brown"];
-        
-        new Chart(ctx1, {
-        type: "bar",
-        data: {
-            labels: xValues,
-            datasets: [{
-            backgroundColor: barColors,
-            data: yValues
-            }]
-        },
-        options: {
-            legend: {display: false},
-            title: {
-            display: true,
-            text: "World Wine Production 2018"
-            }
-        }
-        });
-    }
-    
-    function createPie() {
+    function createGraph() {
         var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
         var yValues = [55, 49, 44, 24, 15];
         var barColors = [
@@ -137,8 +97,8 @@ function createAll() {
         "#1e7145"
         ];
     
-        new Chart(ctx2, {
-        type: "pie",
+        new Chart(ctx, {
+        type: typeOfGraph,
         data: {
             labels: xValues,
             datasets: [{
