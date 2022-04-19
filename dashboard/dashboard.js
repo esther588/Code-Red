e0f7aefa5abda40e96dcf84fd053c5af7e2fc0d9
Esther Cheng <esther588@berkeley.edu>
2022-04-19 10:20:02 -0700

    add graphs

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,7 +1,53 @@
 function createAll() {
     createLine();
-    createBar();
-    createPie();
+    var xValues1 = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues1 = [55, 49, 44, 24, 15];
+    var barColors1 = ["red", "green","blue","orange","brown"];
+        
+    new Chart("barGraph", {
+    type: "bar",
+    data: {
+        labels: xValues1,
+        datasets: [{
+            backgroundColor: barColors1,
+            data: yValues1
+        }]
+    },
+    options: {
+        legend: {display: false},
+        title: {
+        display: true,
+        text: "World Wine Production 2018"
+        }
+    }
+    });
+
+    var xValues2 = ["Italy", "France", "Spain", "USA", "Argentina"];
+        var yValues2 = [55, 49, 44, 24, 15];
+        var barColors2 = [
+        "#b91d47",
+        "#00aba9",
+        "#2b5797",
+        "#e8c3b9",
+        "#1e7145"
+        ];
+    
+        new Chart("pieChart", {
+        type: "pie",
+        data: {
+            labels: xValues2,
+            datasets: [{
+            backgroundColor: barColors2,
+            data: yValues2
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "World Wide Wine Production 2018"
+            }
+        }
+        });
   }
   
   function createLine() {
