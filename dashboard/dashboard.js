a3107e29a6bdd7d12f43fd0f16a2d3fcfa55deed
Esther Cheng <esther588@berkeley.edu>
2022-04-19 00:20:18 -0700

    use same id for all graphs

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,84 +1,85 @@
 function createAll() {
     createLine();
+    createBar();
     createPie();
-    createGraph();
   }
-
-function createLine() {
-    var xValues = [100,200,300,400,500,600,700,800,900,1000];
   
-    new Chart("lineGraph", {
-        type: "line",
+  function createLine() {
+      var xValues = [100,200,300,400,500,600,700,800,900,1000];
+    
+      new Chart("graph", {
+          type: "line",
+          data: {
+          labels: xValues,
+          datasets: [{ 
+              data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+              borderColor: "red",
+              fill: false
+          }, { 
+              data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+              borderColor: "green",
+              fill: false
+          }, { 
+              data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+              borderColor: "blue",
+              fill: false
+          }]
+          },
+          options: {
+          legend: {display: false}
+          }
+      });
+    }
+  
+    function createBar() {
+        var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+        var yValues = [55, 49, 44, 24, 15];
+        var barColors = ["red", "green","blue","orange","brown"];
+        
+        new Chart("graph", {
+        type: "bar",
         data: {
-        labels: xValues,
-        datasets: [{ 
-            data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-            borderColor: "red",
-            fill: false
-        }, { 
-            data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-            borderColor: "green",
-            fill: false
-        }, { 
-            data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-            borderColor: "blue",
-            fill: false
-        }]
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
         },
         options: {
-        legend: {display: false}
+            legend: {display: false},
+            title: {
+            display: true,
+            text: "World Wine Production 2018"
+            }
         }
-    });
-  }
-  function createBar() {
-      var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-  var yValues = [55, 49, 44, 24, 15];
-  var barColors = ["red", "green","blue","orange","brown"];
-  
-  new Chart("barGraph", {
-    type: "bar",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
-    },
-    options: {
-      legend: {display: false},
-      title: {
-        display: true,
-        text: "World Wine Production 2018"
-      }
+        });
     }
-  });
-  }
-  
-  function createPie() {
-      var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-      var yValues = [55, 49, 44, 24, 15];
-      var barColors = [
-      "#b91d47",
-      "#00aba9",
-      "#2b5797",
-      "#e8c3b9",
-      "#1e7145"
-      ];
-  
-      new Chart("pieChart", {
-      type: "pie",
-      data: {
-          labels: xValues,
-          datasets: [{
-          backgroundColor: barColors,
-          data: yValues
-          }]
-      },
-      options: {
-          title: {
-          display: true,
-          text: "World Wide Wine Production 2018"
-          }
-      }
-      });
-  }
+    
+    function createPie() {
+        var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+        var yValues = [55, 49, 44, 24, 15];
+        var barColors = [
+        "#b91d47",
+        "#00aba9",
+        "#2b5797",
+        "#e8c3b9",
+        "#1e7145"
+        ];
+    
+        new Chart("graph", {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "World Wide Wine Production 2018"
+            }
+        }
+        });
+    }
