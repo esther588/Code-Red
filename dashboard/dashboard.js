cebd260a34550aed2370cee93640996827521eea
Esther Cheng <esther588@berkeley.edu>
2022-04-18 23:06:55 -0700

    add bar graph

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -3,12 +3,59 @@ var isChecked = false;
 for ( var i = 0; i < radios.length; i++) {
     if(radios[i].checked) {
         var selected = radios[i].value;
-        if(typeof selected == "opt-4") {
-          createPie();
+        if(typeof selected == "opt-1") {
+            createPie();
+        } else if(typeof selected == "opt-2") {
+            createPie();
+        } else if(typeof selected == "opt-3") {
+            createBar();
+        } else if(typeof selected == "opt-4") {
+            createPie();
         }
         isChecked = true;
         break;
     }
+}
+
+function createBar() {
+    const labels = Utils.months({count: 7});
+    const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+    };
+    new Chart("barGraph", {
+        type: 'bar',
+        data: data,
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        },
+    });
 }
 
 function createPie() {
