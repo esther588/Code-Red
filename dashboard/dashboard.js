94373d198b16789baa9811157e42e39b09420b0c
Esther Cheng <esther588@berkeley.edu>
2022-04-21 09:15:48 -0700

    add create all function and test

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -2,7 +2,7 @@ function checkType(id) {
     var typeOfGraph = "";
 
     if(id == "opt-1") {
-        
+        createAll();
     } else if(id == "opt-2") {
         typeOfGraph = "line";
         createGraph(typeOfGraph);
@@ -15,18 +15,50 @@ function checkType(id) {
     }
 }
 
-function createGraph(type) {
-  document.getElementById("chartContainer").innerHTML = '&nbsp;';
-document.getElementById("chartContainer").innerHTML = '<canvas id="graph" style="width:100%;max-width:600px"></canvas>';
-var elem = document.getElementById("graph").getContext("2d");
-  var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+function createAll() {
+    document.getElementById("chartContainer").innerHTML = '&nbsp;';
+    document.getElementById("chartContainer").innerHTML = '<canvas id="graph" style="width:100%;max-width:600px"></canvas>';
+    var elem = document.getElementById("graph").getContext("2d");
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
     var yValues = [55, 49, 44, 24, 15];
     var barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
+        "#b91d47",
+        "#00aba9",
+        "#2b5797",
+        "#e8c3b9",
+        "#1e7145"
+    ];
+  
+    const newChart = new Chart(elem, {
+    type: "pie",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    options: {
+        title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+        }
+    }
+    });
+}
+
+function createGraph(type) {
+    document.getElementById("chartContainer").innerHTML = '&nbsp;';
+    document.getElementById("chartContainer").innerHTML = '<canvas id="graph" style="width:100%;max-width:600px"></canvas>';
+    var elem = document.getElementById("graph").getContext("2d");
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = [
+        "#b91d47",
+        "#00aba9",
+        "#2b5797",
+        "#e8c3b9",
+        "#1e7145"
     ];
   
     const newChart = new Chart(elem, {
