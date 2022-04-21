5b0098c65127bdff581b856ba28cdedc1a14dfb1
Esther Cheng <esther588@berkeley.edu>
2022-04-21 09:23:58 -0700

    add second graph

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -18,7 +18,9 @@ function checkType(id) {
 function createAll() {
     document.getElementById("chartContainer").innerHTML = '&nbsp;';
     document.getElementById("chartContainer").innerHTML = '<canvas id="graph" style="width:100%;max-width:600px"></canvas>';
+    document.getElementById("chartContainer").innerHTML = '<canvas id="pieChart" style="width:100%;max-width:600px"></canvas>';
     var elem = document.getElementById("graph").getContext("2d");
+    var elem1 = document.getElementById("pieChart").getContext("2d");
     var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
     var yValues = [55, 49, 44, 24, 15];
     var barColors = [
@@ -28,8 +30,25 @@ function createAll() {
         "#e8c3b9",
         "#1e7145"
     ];
-  
+
     const newChart = new Chart(elem, {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "World Wide Wine Production 2018"
+            }
+        }
+    });
+  
+    const newChart1 = new Chart(elem1, {
     type: "pie",
     data: {
         labels: xValues,
