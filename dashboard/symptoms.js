f3d87b08f7b5df104d12ad4bbb67d38a082dc21b
Esther Cheng <esther588@berkeley.edu>
2022-05-09 13:30:38 -0700

    fix error

--- dashboard/symptoms.js
+++ dashboard/symptoms.js
@@ -1,3 +1,34 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+            
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+            
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+            
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
 var optionsMap = new Map();
 var acneCounter = localStorage.getObj('acneCounter');
 var moodCounter = localStorage.getObj('moodCounter');
@@ -95,37 +126,6 @@ function fatigueSubmit() {
             break;
         }
     }
-}
-
-let slideIndex = 1;
-showSlides(slideIndex);
-            
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-            
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-            
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }    
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
 }
 
 for (const dropdown of document.querySelectorAll(".custom-select-wrapper")) {
