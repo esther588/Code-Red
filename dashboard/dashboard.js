5881448ca1acc15745bf635ab28d59e207e6df44
Esther Cheng <esther588@berkeley.edu>
2022-05-09 15:49:46 -0700

    update

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -476,11 +476,3 @@ function showSection(id) {
         curr.style.display = "none";
     }
 }
-
-Storage.prototype.setObj = function(key, obj) {
-    return this.setItem(key, JSON.stringify(obj))
-}
-
-Storage.prototype.getObj = function(key) {
-    return JSON.parse(this.getItem(key))
-}
