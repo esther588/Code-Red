30f5be08c53ebf16bdb7ab0f5216dcd6481d0325
Esther Cheng <esther588@berkeley.edu>
2022-05-09 15:04:07 -0700

    test

--- dashboard/symptoms.js
+++ dashboard/symptoms.js
@@ -144,17 +144,17 @@ function acneSubmit() {
     var acneOption = optionsMap.get("Acne");
     var counter = acneCounter[monthIndex];
     if (acneOption.localeCompare("Whiteheads") == 0) {
-        //acneCounter[monthIndex] = counter++;
+        acneCounter[monthIndex] = counter++;
     } else if (acneOption.localeCompare("Blackheads") == 0) {
-        //acneCounter[monthIndex] = counter++;
+        acneCounter[monthIndex] = counter++;
     }  else if (acneOption.localeCompare("Papules") == 0) {
-        //acneCounter[monthIndex] = counter++;
+        acneCounter[monthIndex] = counter++;
     }  else if (acneOption.localeCompare("Nodules") == 0) {
-        //acneCounter[monthIndex] = counter++;
+        acneCounter[monthIndex] = counter++;
     }  else if (acneOption.localeCompare("Cysts") == 0) {
-        //acneCounter[monthIndex] = counter++;
+        acneCounter[monthIndex] = counter++;
     }
-    //localStorage.setObj('acneCounter', acneCounter);
+    localStorage.setObj('acneCounter', acneCounter);
     alert("Your data has been submitted!");
 }
 
