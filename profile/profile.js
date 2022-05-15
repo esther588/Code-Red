9d51dd36ff685278f34cdb11609b9120dfbe577a
Esther Cheng <esther588@berkeley.edu>
2022-05-14 20:26:38 -0700

    edit log out

--- profile/profile.js
+++ profile/profile.js
@@ -2,3 +2,7 @@ document.querySelector("input").onchange = function() {
     var url = URL.createObjectURL(this.files[0]);
     document.getElementById("profile-photo").style.background = "url(" + url + ") center center no-repeat";
 }
+
+function logOutMsg() {
+    alert("You have successfully logged out.");
+}
