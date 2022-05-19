965110b9b0962a65c1aae390abe23738df1d4fa6
Esther Cheng <esther588@berkeley.edu>
2022-05-19 16:08:27 -0700

    edit

--- profile/profile.js
+++ profile/profile.js
@@ -13,21 +13,18 @@ function displayUser() {
 
 function displayFirst() {
     var firstID = document.getElementById("first-name");
-    var currentUser = localStorage.getItem('currentUser');
     var firstName = localStorage.getItem(currentUser + ' firstname');
     firstID.innerHTML = firstName;
 }
 
 function displayLast() {
     var lastID = document.getElementById("last-name");
-    var currentUser = localStorage.getItem('currentUser');
     var lastName = localStorage.getItem(currentUser + ' lastname');
     lastID.innerHTML = lastName;
 }
 
 function displayFull() {
     var fullID = document.getElementById("full-name");
-    var currentUser = localStorage.getItem('currentUser');
     var firstName = localStorage.getItem(currentUser + ' firstname');
     var lastName = localStorage.getItem(currentUser + ' lastname');
     fullID.innerHTML = firstName + " " + lastName;
@@ -35,7 +32,6 @@ function displayFull() {
 
 function displayYear() {
     var yearID = document.getElementById("year");
-    var currentUser = localStorage.getItem('currentUser');
     var year = localStorage.getItem(currentUser + ' year');
     yearID.innerHTML = year;
 }
