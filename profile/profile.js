e7c0912506347da5a5c4fbbfbc428cceb964a6c8
Esther Cheng <esther588@berkeley.edu>
2022-05-20 13:29:12 -0700

    update

--- profile/profile.js
+++ profile/profile.js
@@ -4,29 +4,27 @@ displayLast();
 displayFull();
 displayYear();
 
+var currentUser = localStorage.getItem('currentUser');
+
 function displayUser() {
     var userID = document.getElementById("username");
-    var currentUser = localStorage.getItem('currentUser');
     userID.innerHTML = currentUser;
 }
 
 function displayFirst() {
     var firstID = document.getElementById("first-name");
-    var currentUser = localStorage.getItem('currentUser');
     var firstName = localStorage.getItem(currentUser + ' firstname');
     firstID.innerHTML = firstName;
 }
 
 function displayLast() {
     var lastID = document.getElementById("last-name");
-    var currentUser = localStorage.getItem('currentUser');
     var lastName = localStorage.getItem(currentUser + ' lastname');
     lastID.innerHTML = lastName;
 }
 
 function displayFull() {
     var fullID = document.getElementById("full-name");
-    var currentUser = localStorage.getItem('currentUser');
     var firstName = localStorage.getItem(currentUser + ' firstname');
     var lastName = localStorage.getItem(currentUser + ' lastname');
     fullID.innerHTML = firstName + " " + lastName;
@@ -34,7 +32,6 @@ function displayFull() {
 
 function displayYear() {
     var yearID = document.getElementById("year");
-    var currentUser = localStorage.getItem('currentUser');
     var year = localStorage.getItem(currentUser + ' year');
     yearID.innerHTML = year;
 }
@@ -42,7 +39,6 @@ function displayYear() {
 document.querySelector("input").onchange = function() {
     var url = URL.createObjectURL(this.files[0]);
     document.getElementById("profile-photo").style.background = "url(" + url + ") center center no-repeat";
-    localStorage.setItem(currentUser + "url", url);
 }
 
 function logOutMsg() {
