a94d9b762612759bd7d99478b3194c49adb0b479
Esther Cheng <esther588@berkeley.edu>
2022-05-23 13:28:47 -0700

    edit user

--- profile/profile.js
+++ profile/profile.js
@@ -4,27 +4,29 @@ displayLast();
 displayFull();
 displayYear();
 
-var currentUser = localStorage.getItem('currentUser');
-
 function displayUser() {
     var userID = document.getElementById("username");
+    var currentUser = localStorage.getItem('currentUser');
     userID.innerHTML = currentUser;
 }
 
 function displayFirst() {
     var firstID = document.getElementById("first-name");
+    var currentUser = localStorage.getItem('currentUser');
     var firstName = localStorage.getItem(currentUser + ' firstname');
     firstID.innerHTML = firstName;
 }
 
 function displayLast() {
     var lastID = document.getElementById("last-name");
+    var currentUser = localStorage.getItem('currentUser');
     var lastName = localStorage.getItem(currentUser + ' lastname');
     lastID.innerHTML = lastName;
 }
 
 function displayFull() {
     var fullID = document.getElementById("full-name");
+    var currentUser = localStorage.getItem('currentUser');
     var firstName = localStorage.getItem(currentUser + ' firstname');
     var lastName = localStorage.getItem(currentUser + ' lastname');
     fullID.innerHTML = firstName + " " + lastName;
@@ -32,6 +34,7 @@ function displayFull() {
 
 function displayYear() {
     var yearID = document.getElementById("year");
+    var currentUser = localStorage.getItem('currentUser');
     var year = localStorage.getItem(currentUser + ' year');
     yearID.innerHTML = year;
 }
